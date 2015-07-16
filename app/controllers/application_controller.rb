d8433af8781acf83e043fcc8ee0b3d3591702f3b
giancarlodangond <giancarlodangond@hotmail.com>
2015-07-16 18:52:26 +0000

    only css left

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,24 +14,50 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/' do  
+    erb :amount_of_options
+  end
+  
+  post '/amoo' do
+    @categories = Category.all 
+    @amount_of_options = params[:amount_of_options].to_i
+    erb :index
+  end
+  
+  get '/index' do
     @categories = Category.all 
     erb :index
   end
   
-  post '/' do
+  post '/index' do
     Category.create(:name => params[:suggestion])
-    redirect '/'
+    redirect '/index'
   end
   
   post '/results' do
     @category = Category.find_by(:name => params[:category])
     @end_suggestion = @category.ideas[rand(@category.ideas.length)]
+    options = []
+    if params[:option1]
+      options << params[:option1]
+    end
+     if params[:option2]
+      options << params[:option2]
+    end
+     if params[:option3]
+      options << params[:option3]
+    end
+     if params[:option4]
+      options << params[:option4]
+    end
+     if params[:option5]
+      options << params[:option5]
+    end
     @o1 = params[:option1]
     @o2 = params[:option2]
     @o3 = params[:option3]
     @o4 = params[:option4]
     @o5 = params[:option5]
-    @item = make_decision(@o1, @o2, @o3, @o4, @o5)
+    @item = make_decision(options)
     @idea1 = Idea.find_or_create_by(:name => @o1)
     @idea2 = Idea.find_or_create_by(:name => @o2)
     @idea3 = Idea.find_or_create_by(:name => @o3)
--- app/models/decision_maker.rb
+++ app/models/decision_maker.rb
@@ -1,7 +1,6 @@
 require_relative "../../config/environment"
 
-def make_decision(o1, o2, o3, o4, o5)
-  options = [o1, o2, o3, o4, o5]
+def make_decision(options)
   item = options[rand(options.length)]
 end
 
